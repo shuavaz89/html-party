9a38224a2d27d2773a1a65500a690c13d6c5c381
Josh Vazquez <joshvaz89@gmail.com>
2024-08-12 20:38:11 +0000

    Setup collapsible and fixed auto party

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 function compileScript(dest) {
-    gulp.src(['./lib/constants.js', './lib/utils.js', /*'./lib/ytplayer.js', */'./lib/index.js'])
+    gulp.src(['./lib/constants.js', './lib/utils.js', './lib/ui.js' , /*'./lib/ytplayer.js', */'./lib/index.js'])
     .pipe(concat('musicwithstyle.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest(dest))
