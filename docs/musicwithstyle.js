96220c99f318b0bf510b26a6b9b6958c12dd4e92
Josh Vazquez <joshvaz89@gmail.com>
2024-08-16 16:09:12 +0000

    Setup YT Player

--- docs/musicwithstyle.js
+++ docs/musicwithstyle.js
@@ -1,3 +1,482 @@
+const PARTY_ELEMENTS = [
+    {
+        element: "select",
+        nodeName: "SELECT",
+    },
+    {
+        element: "textarea",
+        nodeName: "TEXTAREA",
+    },
+    {
+        element: "input",
+        nodeName: "INPUT",
+    },
+    {
+        element: "svg",
+        nodeName: "SVG",
+    },
+    {
+        element: "img",
+        nodeName: "IMG",
+    },
+    {
+        element: "button",
+        nodeName: "BUTTON",
+    },
+    {
+        element: "a",
+        nodeName: "A",
+    },
+    {
+        element: "p",
+        nodeName: "P",
+    },
+    {
+        element: "span",
+        nodeName: "SPAN",
+    },
+    {
+        element: "h1",
+        nodeName: "H1",
+    },
+    {
+        element: "h2",
+        nodeName: "H2",
+    },
+    {
+        element: "h3",
+        nodeName: "H3",
+    },
+    {
+        element: "h4",
+        nodeName: "H4",
+    },
+    {
+        element: "h5",
+        nodeName: "H5",
+    },
+    {
+        element: "h6",
+        nodeName: "H6",
+    },
+    {
+        element: "caption",
+        nodeName: "CAPTION",
+    },
+    {
+        element: "td",
+        nodeName: "TD",
+    },
+];
+
+const FUN_COLORS = [
+    "#ec1254",
+    "#f27c14",
+    "#f5e31d",
+    "#1ee8b6", 
+    "#26a1d5",
+    "#570bb7", "#d042f8", "#2edbef", "#3aefb6", "#f10983",
+    "#d61173", "#fb5cab", "#52c1fa", "#178bd5", "#f5ed16"
+]
+
+const COLOR_FREQ = [
+    "#1e8967", // green
+    "#ffd702", // yellow
+    "#f48d3f", // orange
+    "#d02f4e", // red
+];
+
+const PARTY_TEXT_CLASSES = [
+    "party-time-color",
+    "party-time-textShadow",
+    "party-time-colorVolume", // Color by volume level
+    "party-time-fontSize",
+];
+
+const PARTY_CONTAINER_CLASSES = [
+    "party-time-boxShadow",
+    "party-time-bkgColor",
+    "party-time-height",
+    "party-time-width",
+    "party-time-posTop",
+    "party-time-posRight",
+    "party-time-posBottom",
+    "party-time-posLeft",
+];
+
+const PARTY_TIME_CLASSES = [
+    "party-time-color",
+    "party-time-textShadow",
+    "party-time-boxShadow",
+    "party-time-bkgColor",
+    "party-time-height",
+    "party-time-width",
+    "party-time-colorVolume", // Color by volume level
+    "party-time-fontSize",
+    "party-time-posTop",
+    "party-time-posRight",
+    "party-time-posBottom",
+    "party-time-posLeft",
+];
+
+const PARTY_TIME_FREQ_CLASSES = [
+    "",
+    "party-time-freq-low",
+    "party-time-freq-mid",
+    "party-time-freq-high"
+];
+/**
+ * Remove an array item
+ * @param {array} array 
+ * @param {string|number|boolean} item 
+ */
+function removeArrayItem (array, item) {
+    var index = array.indexOf(item);
+    if (index !== -1) array.splice(index, 1);
+}
+
+/**
+ * Clone an array
+ * @param {array} arr 
+ * @returns {array} Cloned array
+ */
+function cloneArray (arr) {
+    return JSON.parse(JSON.stringify(arr));
+}
+
+/**
+ * Get a random integetory from a range
+ * @param {number} min 
+ * @param {number} max 
+ * @returns {number} Randomg integer
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Convert object to style properties for inline styling.
+ * @param {HTML Element} ele 
+ * @param {object} obj 
+ */
+function objToStyles(ele, obj) {
+    for (var prop in obj) {
+        ele.style.setProperty(prop, obj[prop], 'important');
+    }
+}
+
+/**
+ * Get middle value of a range
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number} Middle Value
+ */
+function middleValue(a, b) {
+    return Math.round((a+b)/2);
+}
+/**
+ * Youtube Streamer
+ */
+// var player;
+// var createYT = function (VIDEO_ID) {
+//     if (VIDEO_ID) {
+//         player = new YT.Player('party-time-ytplayer', {
+//             height: '200', // Set to 0 to hide
+//             width: '200', // Set to 0  to hide
+//             videoId: VIDEO_ID, //'M7lc1UVf-VE',
+//             playerVars: {
+//                 'enablejsapi': 1,
+//                 'playsinline': 1
+//             },
+//             events: {
+//             'onReady': onPlayerReady,
+//             // 'onStateChange': onPlayerStateChange
+//             }
+//         });
+//     }
+// }
+    // 2. This code loads the IFrame Player API code asynchronously.
+    // var tag = document.createElement('script');
+
+    // tag.src = "https://cdn.jsdelivr.net/npm/@thelevicole/youtube-to-html5-loader@5/dist/YouTubeToHtml5.js";
+    // document.body.appendChild(tag);
+    // var firstScriptTag = document.getElementsByTagName('script')[0];
+    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    // function onYouTubeIframeAPIReady() {
+    //     createYT()
+    // }
+
+    // 4. The API will call this function when the video player is ready.
+    // function onPlayerReady(event) {
+    //     var ytVideo = event.target;
+    //     var iframe = ytVideo.getIframe();
+    //     console.log("video", ytVideo);
+    //     // console.log("getVideoEmbedCode", ytVideo.getVideoEmbedCode());
+    //     console.log("iframe", iframe.querySelectorAll("*"));
+    //     // event.target.playVideo();
+    // }
+
+    // 5. The API calls this function when the player's state changes.
+    //    The function indicates that when playing a video (state=1),
+    //    the player should play for six seconds and then stop.
+    // var done = false;
+    // function onPlayerStateChange(event) {
+    //     if (event.data == YT.PlayerState.PLAYING && !done) {
+    //         setTimeout(stopVideo, 6000);
+    //         done = true;
+    //     }
+    // }
+    
+    // function stopVideo() {
+    //     player.stopVideo();
+    // }
+
+var partyTimeVidLink = "";
+var initVidIframe = true;
+
+var setupYTLink = function (event) {
+    var YTLink = event.target.value;
+    // var linkArr = YTLink.split("?v=");
+    partyTimeVidLink = YTLink// ;linkArr[1];
+}
+
+
+// TEST: https://www.youtube.com/watch?v=Eq5a6L7c_UY
+var getYTVid = function () {
+    fetch("https://www.youtube.com/watch?v=Eq5a6L7c_UY")
+        .then(response => console.log(response.json()))
+        .then(data => console.log(data));
+    // var vidYTEle = document.getElementById("party-time-yt-player");
+    // if (vidYTEle) {
+    //     // vidYTEle.setAttribute("src", partyTimeVidLink);
+    //     vidYTEle.setAttribute("data-yt2html5", partyTimeVidLink);
+    //     vidYTEle.setAttribute("controls", "");
+    // } else {
+    //     vidYTEle = document.createElement("video");
+    //     vidYTEle.id = "party-time-yt-player";
+    //     vidYTEle.setAttribute("src", partyTimeVidLink);
+    //     vidYTEle.setAttribute("data-yt2html5", partyTimeVidLink);
+    //     vidYTEle.setAttribute("controls", "");
+    //     document.body.appendChild(vidYTEle)
+    // }
+
+    // new YouTubeToHtml5({
+    //     autoload: true,
+    // });
+
+    // if (initVidIframe) {
+    //     initVidIframe = false;
+    //     createYT(partyTimeVidId);
+    // } else {
+    //     player.loadVideoById(partyTimeVidId);   
+    // }
+
+    // setTimeout(() => {
+    //     const frame = document.getElementById('party-time-ytplayer');
+    //     frame.contentWindow.postMessage("", document.URL);
+    //     var ytIframeEle = document.getElementById("party-time-ytplayer");
+    //     // var iframeWindow = ytIframeEle.contentWindow;
+    //     // var ytVidEle = iframeWindow.document.querySelector("video.video-stream");
+    //     // console.log("ele", ytVidEle)
+    // }, 6000);
+    
+    // createAudioStream("https://www.youtube.com/embed/" + partyTimeVidId);
+}
+function generateUI() {
+    const partyTimeContainer = document.createElement("div");
+    partyTimeContainer.id = "party-time-container";
+    partyTimeContainer.classList = "party-time-controls";
+    partyTimeContainer.style.setProperty('z-index', '999999', 'important');
+    partyTimeContainer.style.setProperty('display', 'flex', 'important');
+    partyTimeContainer.style.setProperty('flex-direction', 'column', 'important');
+    partyTimeContainer.style.setProperty('color', 'white', 'important');
+    partyTimeContainer.style.setProperty('font-size', '12px', 'important');
+    partyTimeContainer.style.setProperty('position', 'fixed', 'important');
+    partyTimeContainer.style.setProperty('top', '16px', 'important');
+    partyTimeContainer.style.setProperty('right', '16px', 'important');
+    partyTimeContainer.style.setProperty('background-color', 'rgba(0,0,0,0.24)', 'important');
+    partyTimeContainer.style.setProperty('padding', '8px', 'important');
+    partyTimeContainer.style.setProperty('border-radius', '8px', 'important');
+
+    var mainEle = document.createElement("div");
+    mainEle.classList.add("party-time-controls");
+
+    var buttonEle = document.createElement("button");
+    buttonEle.id = "party-time-button";
+    buttonEle.classList.add("party-time-controls");
+    buttonEle.innerText = "🎉 Party Time!";
+    buttonEle.onclick = startParty;
+    objToStyles(buttonEle, {
+        "margin-right": "8px",
+        color: "white",
+        cursor: "pointer",
+        "background-color": "purple",
+        padding: "8px",
+        border: "1px solid violet",
+        "border-radius": "8px",
+        "font-size": "12px",
+    });
+    mainEle.appendChild(buttonEle);
+
+    var collapseBtnEle = document.createElement("button");
+    collapseBtnEle.id = "party-time-collapse-btn";
+    collapseBtnEle.classList.add("party-time-controls");
+    collapseBtnEle.innerText = "v";
+    collapseBtnEle.onclick = togglePartyTimeOptions;
+    objToStyles(collapseBtnEle, {
+        background: "rgba(0, 0, 0, 0.87)",
+        cursor: "pointer",
+        border: "0",
+        "border-radius": "24px",
+        padding: "5px 10px",
+        "font-weight": "bold",
+        "font-size": "18px",
+        transform: "rotate(0deg)",
+        color: "white",
+    });
+    mainEle.appendChild(collapseBtnEle);
+
+    var optionEle = document.createElement("div");
+    optionEle.id = "party-time-collapsible";
+    optionEle.classList.add("party-time-controls");
+    optionEle.style.setProperty('display', 'block', 'important');
+    optionEle.style.setProperty('padding', "8px", "important");
+
+    var audioOptionContainerEle = document.createElement("div");
+    audioOptionContainerEle.classList.add("party-time-controls");
+    objToStyles(audioOptionContainerEle, {
+        display: "flex",
+        padding: "4px",
+        "margin-bottom": "16px"
+    })
+
+    var YTOptBtnEle = document.createElement("button");
+    YTOptBtnEle.id = "party-time-yt-option-btn";
+    YTOptBtnEle.classList.add("party-time-controls");
+    YTOptBtnEle.innerText = "Youtube Link";
+    YTOptBtnEle.onclick = function () {
+        toggleAudioOption("yt");
+    }
+    objToStyles(YTOptBtnEle, {
+        padding: "4px",
+        cursor: "pointer",
+        border: "1px solid black",
+        "border-top-left-radius": "25px",
+        "border-bottom-left-radius": "25px",
+        "border-left": "0",
+        "background-color": "purple",
+        color: "white",
+    });
+    audioOptionContainerEle.appendChild(YTOptBtnEle);
+
+    var audioFileOptBtnEle = document.createElement("button");
+    audioFileOptBtnEle.id = "party-time-audio-file-option-btn";
+    audioFileOptBtnEle.classList.add("party-time-controls");
+    audioFileOptBtnEle.innerText = "Upload File";
+    audioFileOptBtnEle.onclick = function () {
+        toggleAudioOption("file");
+    }
+    objToStyles(audioFileOptBtnEle, {
+        padding: "4px",
+        cursor: "pointer",
+        border: "1px solid black",
+        "border-top-right-radius": "25px",
+        "border-bottom-right-radius": "25px",
+        "border-right": "0",
+        "background-color": "white",
+        color: "black",
+    });
+    audioOptionContainerEle.appendChild(audioFileOptBtnEle);
+    
+    optionEle.appendChild(audioOptionContainerEle);
+
+    var ytPlayerContainerEle = document.createElement("div");
+    ytPlayerContainerEle.id = "party-time-ytplayer-container";
+    ytPlayerContainerEle.classList.add("party-time-controls");
+
+    var ytInputField = document.createElement("input");
+    ytInputField.classList.add("party-time-controls");
+    ytInputField.setAttribute("placeholder", "Paste youtube link");
+    ytInputField.oninput = setupYTLink;
+    ytPlayerContainerEle.appendChild(ytInputField);
+
+    var getYtVidBtnEle = document.createElement("button");
+    getYtVidBtnEle.classList.add("party-time-controls");
+    getYtVidBtnEle.innerText = "Get Video";
+    getYtVidBtnEle.onclick = getYTVid;
+    ytPlayerContainerEle.appendChild(getYtVidBtnEle);
+
+    // YT Player
+    var ytPlayerEle = document.createElement("div");
+    ytPlayerEle.id = "party-time-ytplayer";
+    ytPlayerEle.classList.add("party-time-controls");
+    objToStyles(ytPlayerEle, {
+        display: "block",
+    })
+    ytPlayerContainerEle.appendChild(ytPlayerEle);
+
+    optionEle.appendChild(ytPlayerContainerEle);
+
+    var fileUploadEle = document.createElement("input");
+    fileUploadEle.setAttribute("type", "file");
+    fileUploadEle.id = "party-upload";
+    fileUploadEle.classList.add("party-time-controls");
+    fileUploadEle.onchange = handleAudioFile;
+    objToStyles(fileUploadEle, {
+        width: "150px",
+        display: "none",
+        "margin-right": "8px",
+        "font-size": "12px",
+        "background-color": "black",
+        color: "white",
+    });
+    optionEle.appendChild(fileUploadEle);
+
+    var autoPartyLabelEle = document.createElement("label");
+    autoPartyLabelEle.setAttribute("for", "party-auto-party")
+    autoPartyLabelEle.classList.add("party-time-controls");
+    autoPartyLabelEle.innerText = "Auto Party";
+    objToStyles(autoPartyLabelEle, {
+        "font-weight": "bold",
+        color: "white",
+        "font-size": "12px"
+    });
+    optionEle.appendChild(autoPartyLabelEle);
+
+    var autoPartyEle = document.createElement("input")
+    autoPartyEle.setAttribute("type", "checkbox");
+    autoPartyEle.id = "party-auto-party";
+    autoPartyEle.classList.add("party-time-controls");
+    autoPartyEle.onchange = handleAutoParty;
+    optionEle.appendChild(autoPartyEle);
+
+    var partyDebugLabelEle = document.createElement("label");
+    partyDebugLabelEle.setAttribute("for", "party-debug")
+    partyDebugLabelEle.classList.add("party-time-controls");
+    partyDebugLabelEle.innerText = "Show Freq";
+    objToStyles(partyDebugLabelEle, {
+        "font-weight": "bold",
+        color: "white",
+        "font-size": "12px"
+    });
+    optionEle.appendChild(partyDebugLabelEle);
+
+    var partyDebugEle = document.createElement("input");
+    partyDebugEle.setAttribute("type", "checkbox");
+    partyDebugEle.id = "party-debug";
+    partyDebugEle.classList.add("party-time-controls");
+    partyDebugEle.onchange = handleDebug;
+    optionEle.appendChild(partyDebugEle);
+    
+    partyTimeContainer.appendChild(mainEle);
+    partyTimeContainer.appendChild(optionEle);
+    document.body.appendChild(partyTimeContainer);
+}
 function addPartyClasses(ele, classes) {
     var partyArray = cloneArray(classes);
     var partyClass = partyArray[getRandomInt(0, partyArray.length - 1)];
--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,21 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-function compileScript(dest) {
-    gulp.src(['./lib/constants.js', './lib/utils.js', './lib/ui.js' , /*'./lib/ytplayer.js', */'./lib/index.js'])
-    .pipe(concat('musicwithstyle.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(dest))
+function compileScript(dest, minify) {
+    if (minify) {
+        gulp.src(['./lib/constants.js', './lib/utils.js', './lib/ytplayer.js', './lib/ui.js' , './lib/index.js'])
+        .pipe(concat('musicwithstyle.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dest))
+    } else {
+        gulp.src(['./lib/constants.js', './lib/utils.js', './lib/ytplayer.js', './lib/ui.js' , './lib/index.js'])
+        .pipe(concat('musicwithstyle.js'))
+        .pipe(gulp.dest(dest))
+    }
 }
 
 function compile(done) {
-    compileScript('./dist/');
+    compileScript('./dist/', true);
     compileScript('./docs/');
     done();
 }
